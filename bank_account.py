7db1dcd895d3240423e8066891b99628db2bf6ee
전은형 <ooooo5h91@gmail.com>
2022-01-20 20:18:50 +0900

    #4

--- bank_account.py
+++ bank_account.py
@@ -3,4 +3,16 @@ class BankAccount:
     # 은행 계좌에는 예금주와 보유금액이 변수로 있음
     def __init__(self, user_name, money):
         self.user_name = user_name
-        self.money = money
+        self.money =money
+        
+        
+    # money변수는 조회/변경 모두 추가적인 문장을 print해주자
+    @property
+    def money(self):   # money변수의 조회시에 대한 추가 기능 추가한것 (getter)
+        print('누군가 금액을 조회합니다.')
+        return self._money
+    
+    @money.setter
+    def money(self, input_money):   # money변수에 값 대입시에 자동 실행될 코드를 추가(setter)
+        print('누군가 금액을 변경합니다.')
+        self._money = input_money
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from bank_account import BankAccount
 
 accout1 = BankAccount('전은형', 150000)
 
-accout1.money = -100000 # 누군가가 해킹했다
+accout1.money = -100000 # 누군가가 해킹했다 => 값만 바뀌고, 무슨 일이 있었는지 알아내기가 어렵다.
 
 print('보유금액', accout1.money)
